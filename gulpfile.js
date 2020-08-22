43bb33420532a0a76c215cc2e106d6d822938f0e
Nick Amoscato <nick@amoscato.com>
2020-08-22 10:40:28 -0400

    Remove CSS build process; configure stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const cleanCss = require('gulp-clean-css');
-const concat = require('gulp-concat');
 const config = require('config');
 const gulp = require('gulp');
 const log = require('fancy-log');
-const sass = require('gulp-sass');
 const tslint = require('gulp-tslint');
 const typescript = require('gulp-typescript');
 const vinylFtp = require('vinyl-ftp');
@@ -18,13 +15,6 @@ const gulpConfig = {
             'public/.htaccess',
             'public/**/*.*',
             '!public/data/*',
-        ],
-    },
-    css: {
-        dest: 'static/css',
-        src: [
-            'node_modules/normalize.css/normalize.css',
-            'src/css/app.scss',
         ],
     },
     js: {
@@ -50,14 +40,6 @@ function lint() {
         .pipe(typescript.createProject('tsconfig.json')());
 }
 
-function css() {
-    return gulp.src(gulpConfig.css.src)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(concat('all.css'))
-        .pipe(cleanCss({ specialComments: 0 }))
-        .pipe(gulp.dest(gulpConfig.css.dest));
-}
-
 function deploy() {
     const ftpConnection = vinylFtp.create({
         host: config.get('ftp.host'),
@@ -74,14 +56,12 @@ function deploy() {
 
 function watch() {
     gulp.watch(Object.values(gulpConfig.js.src), lint);
-    gulp.watch('src/css/**/*.scss', css);
 
     return js(true); // use webpack watch
 }
 
-exports.all = gulp.parallel(js, css);
-exports.css = css;
-exports.default = gulp.parallel(css, watch);
+exports.all = js;
+exports.default = watch;
 exports.deploy = deploy;
 exports.js = js;
 exports.lint = lint;
