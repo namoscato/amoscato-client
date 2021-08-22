4f74d5119dad7719cfb0151082d11e07d90b350c
Nick Amoscato <nick@amoscato.com>
2021-08-22 15:57:13 -0400

    Move to Hugo Pipes' ESBuild

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,7 @@ delete webpack.output;
 module.exports = function (config) {
     config.set({
         browsers: ["ChromeHeadless"],
-        files: ["node_modules/moment/moment.js", "src/**/*.spec.ts"],
+        files: ["node_modules/moment/moment.js", "assets/js/**/*.spec.ts"],
         frameworks: ["jasmine", "webpack"],
         preprocessors: {
             "**/*.ts": ["webpack"],
